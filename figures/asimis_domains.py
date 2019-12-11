fd85b20c03d3252d16bd3fb64cb3b956774f4a28
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2019-12-11 09:01:03 +0100

    Add North Asia domain locations (from Wikipedia).

--- figures/asimis_domains.py
+++ figures/asimis_domains.py
@@ -16,6 +16,44 @@ import absplots as apl
 import util
 
 
+DOMAINS = {
+    'Altai':          (92, 48, [-100e3, 100e3, -100e3, 100e3]),
+    'Putorana':       (94, 69, [-100e3, 100e3, -100e3, 100e3]),
+    'Sayan':          (98, 53, [-100e3, 100e3, -100e3, 100e3]),
+    'Khangai':       (100, 49, [-100e3, 100e3, -100e3, 100e3]),
+    'Transbaikal':   (117, 57, [-100e3, 100e3, -100e3, 100e3]),
+    'Stanovoy':      (126, 56, [-100e3, 100e3, -100e3, 100e3]),
+    'Verkhoyansk':   (129, 67, [-100e3, 100e3, -100e3, 100e3]),
+    'YamAlin':       (135, 54, [-100e3, 100e3, -100e3, 100e3]),
+    'Kolyma':        (159, 63, [-100e3, 100e3, -100e3, 100e3]),
+    'Chersky':       (143, 64, [-100e3, 100e3, -100e3, 100e3]),
+    'Kamchatka':     (160, 55, [-100e3, 100e3, -100e3, 100e3]),
+    'Anadyr':        (170, 67, [-100e3, 100e3, -100e3, 100e3]),
+    'Koryak':        (172, 62, [-100e3, 100e3, -100e3, 100e3]),
+    'Chukotka':      (177, 68, [-100e3, 100e3, -100e3, 100e3]),
+    'Brooks':       (-152, 68, [-100e3, 100e3, -100e3, 100e3]),
+    'Ahklun':       (-161, 59, [-100e3, 100e3, -100e3, 100e3])}
+
+
+def draw_model_domain(ax, extent, **kwargs):
+    """Draw model domain with projection axes."""
+    west, east, south, north = extent
+    ax.plot([west, east, east, west, west],
+            [south, south, north, north, south], **kwargs)
+    ax.plot([0, 0], [south, north], lw=0.5, **kwargs)
+    ax.plot([west, east], [0, 0], lw=0.5, **kwargs)
+    ax.plot(0, 0, marker='o', **kwargs)
+
+
+def draw_model_domains(ax, domains, **kwargs):
+    """Draw multiple model domains."""
+    for name, (lon, lat, extent) in domains.items():
+        proj = ccrs.LambertAzimuthalEqualArea(
+            central_longitude=lon, central_latitude=lat)
+        draw_model_domain(ax, extent, transform=proj, **kwargs)
+        ax.text(0, 0, name, fontsize=4, transform=proj)
+
+
 def draw_shapefile(filename, ax=None, crs=None, **kwargs):
     """Add shapefile geometries without duplicates."""
     ax = ax or plt.gca()
@@ -64,6 +102,7 @@ def main():
                    crs=ccrs.LambertAzimuthalEqualArea(central_latitude=90))
     draw_shapefile('../data/external/LGM_best_estimate.shp',
                    ax=ax, alpha=0.75, facecolor='C2')
+    draw_model_domains(ax=ax, domains=DOMAINS, color='C3', zorder=3)
 
     # add legend
     add_legend(
