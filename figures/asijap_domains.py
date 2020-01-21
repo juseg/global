bb775ffbed79bc6ec71810016da2b3d896d94d16
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2020-01-21 14:58:54 +0100

    Add Japan location map on Japan domains figure.

--- figures/asijap_domains.py
+++ figures/asijap_domains.py
@@ -20,44 +20,45 @@ DOMAINS = {
     'Akaishi':       (138, 36, [+10e3, +30e3, -40e3, -20e3]),
     'Hidaka':        (143, 43, [-30e3, -10e3, -40e3, -20e3])}
 
+REGIONS = {
+    'Chubu':    (138, 36, [-070e3, 080e3, -095e3, 105e3], [.0, .0, .5, 1.]),
+    'Hokkaido': (143, 43, [-100e3, 050e3, -110e3, 090e3], [.5, .0, .5, 1.]),
+    'Japan':    (140, 40, [-500e3, 500e3, -750e3, 750e3], [.3, .4, .4, .5])}
+
 
 def main():
     """Main program called during execution."""
 
     # initialize figure
     fig = apl.figure_mm(figsize=(180, 120))
-    ax0 = fig.add_axes(
-        [0, 0, 0.5, 1], projection=ccrs.LambertAzimuthalEqualArea(
-            central_longitude=138, central_latitude=36))
-    ax1 = fig.add_axes(
-        [0.5, 0, 0.5, 1], projection=ccrs.LambertAzimuthalEqualArea(
-            central_longitude=143, central_latitude=43))
-    ax0.set_extent([-75e3, 75e3, -100e3, 100e3], crs=ax0.projection)
-    ax1.set_extent([-75e3, 75e3, -100e3, 100e3], crs=ax1.projection)
+    grid = dict()
+    for region, (lon, lat, extent, pos) in REGIONS.items():
+        ax = fig.add_axes(pos, projection=ccrs.LambertAzimuthalEqualArea(
+            central_longitude=lon, central_latitude=lat))
+        ax.set_extent(extent, crs=ax.projection)
 
-    # add etopo1bed background
-    csr.add_topography('../data/external/srtm_chubu.tif',
-                       ax=ax0, cmap='Greys', vmax=4500)
-    csr.add_topography('../data/external/srtm_hokkaido.tif',
-                       ax=ax1, cmap='Greys', vmax=4500)
+        # add etopo1bed background
+        csr.add_topography('../data/external/{}_{}.tif'.format(
+            'ETOPO1_Bed_c_geotiff' if region == 'Japan' else 'srtm',
+            region.lower()), ax=ax, cmap='Greys', vmax=4500)
 
-    # add physical elements
-    for ax in (ax0, ax1):
-        cne.add_rivers(ax=ax, edgecolor='0.25', scale='10m')
-        cne.add_lakes(ax=ax, edgecolor='0.25', facecolor='0.75', scale='10m')
-        cne.add_coastline(ax=ax, edgecolor='0.25', scale='10m')
-        cne.add_graticules(ax=ax, interval=1, scale='10m')
+        # add physical elements
+        scale = '50m' if region == 'Japan' else '10m'
+        cne.add_rivers(ax=ax, edgecolor='0.25', scale=scale)
+        cne.add_lakes(ax=ax, edgecolor='0.25', facecolor='0.75', scale=scale)
+        cne.add_coastline(ax=ax, edgecolor='0.25', scale=scale)
+        cne.add_graticules(ax=ax, interval=5, scale=scale)
 
         # add glaciers and domains
         cne.add_shapefile('../data/external/lgm.shp',
                           ax=ax, alpha=0.75, facecolor='C0')
-        util.draw_model_domains(ax=ax, domains=DOMAINS, color='C3', zorder=3)
+        util.draw_model_domains(ax=ax, domains=DOMAINS, grid=False, color='C3')
 
     # add legend
-    ax.legend([mpl.patches.Patch(facecolor='C0', alpha=0.75),
-               mpl.patches.Patch(facecolor='none', edgecolor='C3')],
-              ['MIS 4 (Ehlers et al., 2011)', 'Planned model domains'],
-              loc='upper right')
+    fig.legend([mpl.patches.Patch(facecolor='C0', alpha=0.75),
+                mpl.patches.Patch(facecolor='none', edgecolor='C3')],
+               ['MIS 4 (Ehlers et al., 2011)', 'Planned model domains'],
+               loc='upper right')
 
     # save
     util.savefig(fig)
