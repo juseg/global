c48def83d4b76d7c895a5f078360e6a7e28a512c
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2024-04-30 17:04:38 +0200

    Move ERA5 code to glopdd.py.

--- data/glopdd.py
+++ data/glopdd.py
@@ -3,16 +3,19 @@
 # Creative Commons Attribution-ShareAlike 4.0 International License
 # (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
 
-"""Compute global mass balance from CHELSA climatology."""
+"""Compute glacial inception threshold from global climatologies."""
 
 import os.path
-import dask.diagnostics
+import cdsapi
 import dask.distributed
 import numpy as np
 import scipy.special as sc
 import xarray as xr
 import hyoga
 
+
+# Aggregate climatologies
+# -----------------------
 
 def write_climatology(source='chelsa'):
     """Write global climatology to disk, return file path."""
@@ -35,6 +38,122 @@ def write_climatology(source='chelsa'):
     # return file path
     return filepath
 
+
+def aggregate_era5_avg(var='t2m', start=1981, end=2010):
+    """Compute ERA5 multiyear monthly averages from monthly means."""
+
+    # if file exists, return path
+    filepath = \
+        f'external/era5/clim/era5.{var}.mon.{start%100:d}{end%100:d}.avg.nc'
+    if os.path.isfile(filepath):
+        return filepath
+
+    # compute monthly mean
+    print(f"Computing {filepath} ...")
+    paths = [
+        download_era5_monthly(year, var=var) for year in range(start, end+1)]
+    with xr.open_mfdataset(paths) as ds:
+        ds.groupby('time.month').mean().to_netcdf(
+            filepath, encoding={var: {'zlib': True}})
+
+    # return file path
+    return filepath
+
+
+def aggregate_era5_std(freq='day', start=1981, end=2010):
+    """Compute ERA5 multiyear monthly standard deviation from frequency."""
+
+    # if file exists, return path
+    filepath = (
+        f'external/era5/clim/era5.t2m.{freq}.{start%100:d}{end%100:d}.std.nc')
+    if os.path.isfile(filepath):
+        return filepath
+
+    # compute monthly standard deviation
+    func = download_era5_daily if freq == 'day' else download_era5_hourly
+    paths = [func(a, m) for m in range(1, 13) for a in range(start, end+1)]
+    with dask.distributed.Client():
+        with xr.open_mfdataset(paths, chunks={'latitude': 103}) as ds:
+            print(f"Computing {filepath} ...")
+            ds.groupby('time.month').std('time').to_netcdf(
+                filepath, encoding={'t2m': {'zlib': True}})
+
+    # return file path
+    return filepath
+
+
+# Download weather data
+# ---------------------
+
+def download_era5_daily(year, month):
+    """Download ERA5 daily means for a single month."""
+
+    # if file exists, return path
+    filepath = f'external/era5/daily/era5.t2m.day.{year:d}.{month:02d}.nc'
+    if os.path.isfile(filepath):
+        return filepath
+
+    # query daily stats application
+    client = cdsapi.Client()
+    result = client.service('tool.toolbox.orchestrator.workflow', params={
+        'realm': 'user-apps', 'project': 'app-c3s-daily-era5-statistics',
+        'version': 'master', 'workflow_name': 'application', 'kwargs': {
+            'dataset': 'reanalysis-era5-single-levels',
+            'variable': '2m_temperature', 'statistic': 'daily_mean',
+            'year': f'{year:d}', 'month': f'{month:02d}'}})
+
+    # download the result
+    print(f"Downloading {filepath} ...")
+    client.download(result, [filepath])
+    return filepath
+
+
+def download_era5_hourly(year, month):
+    """Download ERA5 hourly means for a single month."""
+
+    # if file exists, return path
+    filepath = f'external/era5/hourly/era5.t2m.hour.{year:d}.{month:02d}.nc'
+    if os.path.isfile(filepath):
+        return filepath
+
+    # else retrieve the file
+    client = cdsapi.Client()
+    client.retrieve(
+        'reanalysis-era5-single-levels', {
+            'product_type': 'reanalysis', 'format': 'netcdf',
+            'variable': '2m_temperature',
+            'year': f'{year:d}', 'month': f'{month:02d}',
+            'day': [f'{i:02d}' for i in range(1, 32)],
+            'time': [f'{i:02d}:00' for i in range(24)]},
+        filepath)
+
+    # return filepath
+    return filepath
+
+
+def download_era5_monthly(year, var='t2m'):
+    """Download ERA5 monthly means for a single month."""
+
+    # if file exists, return path
+    filepath = f'external/era5/monthly/era5.{var}.mon.{year:d}.nc'
+    if os.path.isfile(filepath):
+        return filepath
+
+    # variable name in CDS
+    variable = {'t2m': '2m_temperature', 'tp': 'total_precipitation'}[var]
+
+    # request download from CDS
+    print(f"Downloading {filepath} ...")
+    client = cdsapi.Client()
+    client.retrieve('reanalysis-era5-single-levels-monthly-means', {
+        'format': 'netcdf', 'month': [f'{i}' for i in range(1, 13)],
+        'product_type': 'monthly_averaged_reanalysis', 'time': '00:00',
+        'variable': variable, 'year': f'{year:d}'}, filepath)
+    return filepath
+
+
+# Compute main outputs
+# --------------------
 
 def open_climatology(source='chelsa', freq='day'):
     """Write global mass balance to disk, return file path."""
@@ -105,14 +224,28 @@ def compute_glacial_threshold(smb, source='chelsa'):
     return git
 
 
+# Main program
+# ------------
+
 def main(source='chelsa'):
     """Main program called during execution."""
 
     # use dask distributed
     dask.distributed.Client()
 
-    # create directory if missing
+    # create directories if missing
+    os.makedirs('external/era5/clim', exist_ok=True)
+    os.makedirs('external/era5/daily', exist_ok=True)
+    os.makedirs('external/era5/hourly', exist_ok=True)
+    os.makedirs('external/era5/monthly', exist_ok=True)
+    os.makedirs('external/chelsa/era5/clim', exist_ok=True)
     os.makedirs('processed', exist_ok=True)
+
+    # compute climatologies
+    aggregate_era5_avg(var='t2m')
+    aggregate_era5_avg(var='tp')
+    aggregate_era5_std(freq='day')
+    aggregate_era5_std(freq='hour')
 
     # unless file exists
     filepath = f'processed/glopdd.git.{source}.nc'
