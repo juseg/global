08e5ffbbb1d90259576fc924bab00b964b39df95
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2024-07-24 11:17:08 +0200

    Add glacial inception threshold plot (wip).

--- figures/glopdd_threshold.py
+++ figures/glopdd_threshold.py
@@ -1,14 +1,59 @@
 #!/usr/bin/python
+# Copyright (c) 2023-2024, Julien Seguinot (juseg.dev)
+# Creative Commons Attribution-ShareAlike 4.0 International License
+# (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
 
 """Plot global PDD glacial inception threshold."""
 
-import matplotlib.pyplot as plt
 import xarray as xr
+import absplots as apl
 
-git = xr.open_dataset('../data/processed/glopdd.git.chelsa.nc').git
-# git = git.sel(x=slice(-15, 45), y=slice(60, 30))
-git.attrs.update(long_name='glacial inception threshold (°C)')
-git.plot.imshow(cmap='Spectral')
-plt.title('')
-plt.savefig(__file__[:-3], dpi=300)
-plt.show()
+
+def main():
+    """Main program called during execution."""
+
+    # initialize figure
+    fig = apl.figure_mm(figsize=(180, 87))
+    ax0 = fig.add_axes([0, 0, 1, 1])
+    ax1 = fig.add_axes_mm([10, 25, 25, 25])  # South Pacific
+    ax2 = fig.add_axes_mm([70, 32.5, 25, 25])  # South Atlantic
+    cax = fig.add_axes_mm([125, 35, 30, 2.5])  # Australia
+
+    # plot global inception threshold (FIXME negate in processing?)
+    git = xr.open_mfdataset('../data/processed/glopdd.git.cw5e5.nc').git
+    sel = git.sel(lon=slice(None, None, 10), lat=slice(None, None, 10))
+    sel.notnull().plot.contour(
+        ax=ax0, add_labels=False, colors='0.9', levels=[0], linewidths=5, zorder=-1)
+    # sel.plot.contour(
+    #     ax=ax0, colors='w', levels=[0.5], linestyles=['--'])
+    sel.plot.imshow(
+        ax=ax0, add_labels=False, cmap='plasma_r', cbar_ax=cax, cbar_kwargs={
+            'label': 'glacial inception threshold (K)',
+            'orientation': 'horizontal'})
+    sel = git.sel(lon=slice(-76.5, -70.5), lat=slice(-52, -46))
+    sel.notnull().plot.contour(
+        ax=ax1, add_labels=False, colors='0.9', levels=[0], linewidths=5, zorder=-1)
+    sel.plot(
+        ax=ax1, add_colorbar=False, add_labels=False, cmap='plasma_r')
+    sel = git.sel(lon=slice(7.7, 8.3), lat=slice(46.2, 46.8))
+    sel.notnull().plot.contour(
+        ax=ax2, add_labels=False, colors='0.9', levels=[0], linewidths=5, zorder=-1)
+    sel.plot(
+        ax=ax2, add_colorbar=False, add_labels=False, cmap='plasma_r')
+
+    # set axes properties
+    ax0.set_aspect('equal')
+    ax0.set_xticks(range(-180, 180, 30))
+    ax0.set_yticks(range(-90, 90, 30))
+    for ax in (ax1, ax2):
+        ax.set_aspect('equal')
+        ax.xaxis.set_visible(False)
+        ax.yaxis.set_visible(False)
+    cax.grid(False)
+
+    # save figure
+    fig.savefig(__file__[:-3], dpi=254)
+
+
+if __name__ == '__main__':
+    main()
