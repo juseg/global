b501d95e2ad7a78c559878218d4df9070e46de09
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2024-07-25 16:07:17 +0200

    Rework glacial inception threshold plot.

--- figures/glopdd_threshold.py
+++ figures/glopdd_threshold.py
@@ -6,50 +6,63 @@
 """Plot global PDD glacial inception threshold."""
 
 import xarray as xr
-import absplots as apl
+import matplotlib.pyplot as plt
 
 
 def main():
     """Main program called during execution."""
 
     # initialize figure
-    fig = apl.figure_mm(figsize=(180, 87))
-    ax0 = fig.add_axes([0, 0, 1, 1])
-    ax1 = fig.add_axes_mm([10, 25, 25, 25])  # South Pacific
-    ax2 = fig.add_axes_mm([70, 32.5, 25, 25])  # South Atlantic
-    cax = fig.add_axes_mm([125, 35, 30, 2.5])  # Australia
+    fig = plt.figure(figsize=(160/25.4, 80/25.4))
+    axes = [fig.add_axes(rect) for rect in (
+        [0, 0, 1, 1],
+        [2/36, 5/18, 5/36, 5/18],
+        [14/36, 6.5/18, 5/36, 5/18])]
+    cax = fig.add_axes([17/36, 4.5/18, 5/36, .5/18])
 
-    # plot global inception threshold (FIXME negate in processing?)
-    git = xr.open_mfdataset('../data/processed/glopdd.git.cw5e5.nc').git
-    sel = git.sel(lon=slice(None, None, 10), lat=slice(None, None, 10))
-    sel.notnull().plot.contour(
-        ax=ax0, add_labels=False, colors='0.9', levels=[0], linewidths=5, zorder=-1)
-    # sel.plot.contour(
-    #     ax=ax0, colors='w', levels=[0.5], linestyles=['--'])
-    sel.plot.imshow(
-        ax=ax0, add_labels=False, cmap='plasma_r', cbar_ax=cax, cbar_kwargs={
-            'label': 'glacial inception threshold (K)',
-            'orientation': 'horizontal'})
-    sel = git.sel(lon=slice(-76.5, -70.5), lat=slice(-52, -46))
-    sel.notnull().plot.contour(
-        ax=ax1, add_labels=False, colors='0.9', levels=[0], linewidths=5, zorder=-1)
-    sel.plot(
-        ax=ax1, add_colorbar=False, add_labels=False, cmap='plasma_r')
-    sel = git.sel(lon=slice(7.7, 8.3), lat=slice(46.2, 46.8))
-    sel.notnull().plot.contour(
-        ax=ax2, add_labels=False, colors='0.9', levels=[0], linewidths=5, zorder=-1)
-    sel.plot(
-        ax=ax2, add_colorbar=False, add_labels=False, cmap='plasma_r')
 
-    # set axes properties
-    ax0.set_aspect('equal')
-    ax0.set_xticks(range(-180, 180, 30))
-    ax0.set_yticks(range(-90, 90, 30))
-    for ax in (ax1, ax2):
-        ax.set_aspect('equal')
-        ax.xaxis.set_visible(False)
-        ax.yaxis.set_visible(False)
-    cax.grid(False)
+    # open glacial inception threshold
+    with xr.open_dataarray('../data/processed/glopdd.git.cw5e5.nc') as git:
+        # git = git.where((-12 < git)*(git < 0))
+
+        # for each region
+        for ax, (region, bounds) in zip(axes, {
+                'World': [-180, -90, 180, 90],
+                'Patagonia': [-76.5, -52, -70.5, -46],
+                'Alps': [7.7, 46.2, 8.3, 46.8]}.items()):
+            west, south, east, north = bounds
+
+            # select partial data
+            if region == 'World':
+                sel = git.isel(lat=slice(0, -1, 10), lon=slice(0, -1, 10))
+            else:
+                sel = git.sel(lat=slice(south, north), lon=slice(west, east))
+
+            # plot image map
+            if region == 'World':
+                kwargs = {'cbar_ax': cax, 'cbar_kwargs': {
+                    'label': 'glacial inception threshold (K)',
+                    'orientation': 'horizontal'}}
+            else:
+                kwargs = {'add_colorbar': False}
+            sel.plot.imshow(
+                ax=ax, add_labels=False, cmap='plasma_r', vmin=-20, vmax=0,
+                **kwargs)
+
+            # set axes properties
+            ax.set_aspect('equal')
+            ax.set_ylim(south, north)
+            ax.set_xlim(west, east)
+            if region == 'World':
+                ax.set_xticks(range(-180, 180, 30))
+                ax.set_yticks(range(-90, 90, 30))
+            else:
+                ax.set_title(region)
+                ax.xaxis.set_visible(False)
+                ax.yaxis.set_visible(False)
+
+        # set colorbar axes properties
+        cax.grid(False)
 
     # save figure
     fig.savefig(__file__[:-3], dpi=254)
