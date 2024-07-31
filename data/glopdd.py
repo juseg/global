978035a536772f343896368d3acedcce089b2f6f
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2024-07-31 16:26:04 +0200

    Expand file paths to be more explicit.

--- data/glopdd.py
+++ data/glopdd.py
@@ -355,19 +355,21 @@ def main():
         f'{"n" if (lat >= 0) else "s"}{abs(lat):02d}'
         f'{"e" if (lon >= 0) else "w"}{abs(lon):03d}'
         for lat in range(-90, 90, 30) for lon in range(-180, 180, 30)]
-    paths = [f'processed/glopdd.git.{args.source}.{tile}.nc' for tile in tiles]
+    # FIXME allow custom pdd factor
+    prefix = f'processed/glopdd.git.{args.source}.{args.precip}.pdd3'
+    paths = [f'{prefix}.{tile}.nc' for tile in tiles]
 
     # start distributed client of progress bar
     with Context(**options):
 
         # for each tile and corresponding path
-        for tile, filepath in zip(tiles, paths):
+        for tile, tilepath in zip(tiles, paths):
 
             # unless file exists
-            if args.overwrite or not os.path.isfile(filepath):
+            if args.overwrite or not os.path.isfile(tilepath):
 
                 # compute glacial threshold
-                print(f"Computing {filepath} ...")
+                print(f"Computing {tilepath} ...")
                 temp, prec, stdv = open_climate_tile(
                     tile, freq=args.freq, source=args.source)
                 smb = compute_mass_balance(
@@ -375,7 +377,7 @@ def main():
                     precip=args.precip)
                 git = compute_glacial_threshold(smb)
                 git.astype('f4').to_netcdf(
-                    filepath, encoding={'git': {'zlib': True}})
+                    tilepath, encoding={'git': {'zlib': True}})
 
                 # close files after computation (xarray #4131)
                 for da in temp, prec, stdv:
@@ -385,25 +387,24 @@ def main():
         with xr.open_mfdataset(paths) as ds:
 
             # save compressed geotiff
-            filepath = f'processed/glopdd.git.{args.source}.tif'
-            if args.overwrite or not os.path.isfile(filepath):
-                print(f"Assembling {filepath} ...")
+            if args.overwrite or not os.path.isfile(f'{prefix}.tif'):
+                print(f"Assembling {f'{prefix}.tif'} ...")
                 git = ds.git.rio.set_spatial_dims(x_dim='lon', y_dim='lat')
-                git.rio.to_raster(filepath, compress='LZW', tiled=True)
+                git.rio.to_raster(f'{prefix}.tif', compress='LZW', tiled=True)
 
             # save uncompressed netcdf
-            if args.overwrite or not os.path.isfile(filepath):
-                filepath = f'processed/glopdd.git.{args.source}.nc'
-                print(f"Assembling {filepath} ...")
-                ds.to_netcdf(filepath)
+            if args.overwrite or not os.path.isfile(f'{prefix}.nc'):
+                print(f"Assembling {f'{prefix}.nc'} ...")
+                ds.to_netcdf(f'{prefix}.nc')
 
                 # nccopy compression beats xarray by far
-                print(f"Compressing {filepath} ...")
-                dirname, basename = os.path.split(filepath)
+                print(f"Compressing {f'{prefix}.nc'} ...")
+                dirname, basename = os.path.split(f'{prefix}.nc')
                 with tempfile.NamedTemporaryFile(
                         dir=dirname, prefix=basename+'.') as tmp:
-                    subprocess.run(['nccopy', '-sd6', filepath, tmp.name])
-                    os.replace(tmp.name, filepath)
+                    subprocess.run(
+                        ['nccopy', '-sd6', f'{prefix}.nc', tmp.name])
+                    os.replace(tmp.name, f'{prefix}.nc')
 
 
 if __name__ == '__main__':
