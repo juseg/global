5dd4a1759b339d20e64701b8cb25ad461d3ee7e9
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2024-08-12 16:50:59 +0200

    Cleanup and lint world plot script.

--- figures/glopdd_world.py
+++ figures/glopdd_world.py
@@ -5,10 +5,6 @@
 
 """Plot global PDD glacial inception world map."""
 
-import argparse
-import multiprocessing
-import os.path
-import time
 import xarray as xr
 import matplotlib.pyplot as plt
 from glopdd_threshold import cmaps
@@ -19,9 +15,9 @@ def open_git(source='cw5e5', precip='cp', ddf=3):
     """Open glacial inception threshold."""
     if source == 'fdiff':
         return open_git('cw5e5', precip, 5) - open_git('cw5e5', precip, 2)
-    elif source == 'pdiff':
+    if source == 'pdiff':
         return open_git('cw5e5', 'pp', ddf) - open_git('cw5e5', 'cp', ddf)
-    elif source == 'sdiff':
+    if source == 'sdiff':
         return open_git('cera5', precip, ddf) - open_git('cw5e5', precip, ddf)
     da = xr.open_dataarray(
         f'../data/processed/glopdd.git.{source}.{precip}.ddf{ddf}.nc',
@@ -30,19 +26,7 @@ def open_git(source='cw5e5', precip='cp', ddf=3):
     return da
 
 
-def main():
-    """Main program called during execution."""
-
-    # available data sources
-    sources = ['cera5', 'cw5e5', 'pdiff', 'sdiff']  # fdiff
-    options = {'sources': sources}
-
-    # plot all frames in parallel
-    plotter = glopdd_utils.MultiPlotter(plot, **options)
-    plotter()
-
-
-def plot(source='cw5e5', precip='cp', ddf=3):
+def plot(source='cw5e5'):
     """Make plot and save figure for given arguments."""
 
     # initialize figure
@@ -67,10 +51,10 @@ def plot(source='cw5e5', precip='cp', ddf=3):
         props = {'cmap': cmaps('Oranges', 'Blues'), 'vmin': -20, 'vmax': 0}
 
     # open global inception threshold
-    with open_git(source=source, precip=precip, ddf=ddf) as diff:
+    with open_git(source=source) as da:
 
         # plot global map
-        diff.isel(lat=slice(0, -1, 10), lon=slice(0, -1, 10)).plot.imshow(
+        da.isel(lat=slice(0, -1, 10), lon=slice(0, -1, 10)).plot.imshow(
             ax=ax0, add_labels=False, cbar_ax=cax, cbar_kwargs={
                 'label': label, 'orientation': 'horizontal'}, **props)
 
@@ -89,8 +73,7 @@ def plot(source='cw5e5', precip='cp', ddf=3):
             west, south, east, north = bounds
 
             # plot regional map
-            diff.sel(
-                    lat=slice(south, north), lon=slice(west, east)).plot.imshow(
+            da.sel(lat=slice(south, north), lon=slice(west, east)).plot.imshow(
                 ax=ax, add_colorbar=False, add_labels=False, **props)
 
             # mark inset
@@ -107,13 +90,11 @@ def plot(source='cw5e5', precip='cp', ddf=3):
     return fig
 
 
-def save(source, precip, ddf):
-    """Plot and save figure."""
-    filename = f'{__file__[:-3]}_{source}'
-    print(f"[{time.strftime('%H:%M:%S')}] plotting {os.path.basename(filename)} ...")
-    fig = plot(source, precip, ddf)
-    fig.savefig(filename, dpi='figure')
-    plt.close(fig)
+def main():
+    """Main program called during execution."""
+    sources = ['cera5', 'cw5e5', 'pdiff', 'sdiff']  # fdiff
+    plotter = glopdd_utils.MultiPlotter(plot, sources=sources)
+    plotter()
 
 
 if __name__ == '__main__':
