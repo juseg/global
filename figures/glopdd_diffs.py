75e796214dccbbd3f892b1a62c865ebc914ebbee
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2024-08-19 14:27:02 +0200

    Move colorbars into panel insets.

--- figures/glopdd_diffs.py
+++ figures/glopdd_diffs.py
@@ -15,17 +15,21 @@ def plot(source='sdiff'):
     # initialize figure
     fig, axes = plt.subplots(
         figsize=(160/25.4, 80/25.4), ncols=3, gridspec_kw={
-            'left': 0.5/36, 'right': 35.5/36, 'bottom': 4/18, 'top': 17/18,
-            'wspace': 1/11})
-    cax1 = fig.add_axes([9/36, 2.5/18, 6/36, 0.5/18])
-    cax2 = fig.add_axes([27/36, 2.5/18, 6/36, 0.5/18])
+            'left': 0.5/36, 'bottom': 0.5/18, 'right': 35.5/36,
+            'top': 16.5/18, 'wspace': 1/11})
+    cax1 = fig.add_axes([3/36, 3/18, 6/36, 0.5/18])
+    cax2 = fig.add_axes([27/36, 3/18, 6/36, 0.5/18])
+    for ax in (axes[0], axes[2]):
+        ax.add_patch(plt.Rectangle(
+            (0, 0), 1, 3.5/16, alpha=0.75, ec=str(2/3), fc='w',
+            transform=ax.transAxes))
 
     # open global inception threshold
     with glopdd_utils.open_inception_threshold_duo(source) as (da0, da1):
 
-        # select region (aspect 14x12 = 7x6)
-        da0 = da0.sel(lat=slice(-52, -45), lon=slice(-76, -70))
-        da1 = da1.sel(lat=slice(-52, -45), lon=slice(-76, -70))
+        # select region (aspect 16x11 = 8x5.5)
+        da0 = da0.sel(lat=slice(-54, -46), lon=slice(-76, -70.5))
+        da1 = da1.sel(lat=slice(-54, -46), lon=slice(-76, -70.5))
 
         # plot absolute values
         cmap = glopdd_utils.combine_colormaps('Oranges', 'Blues')
