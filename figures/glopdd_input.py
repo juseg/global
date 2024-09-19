2bbe521159ba8159ba20dedf84eff6360bf9f21a
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2024-09-19 10:37:37 +0200

    Fix small bug in figure makefile.

--- figures/glopdd_input.py
+++ figures/glopdd_input.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright (c) 2023-2024, Julien Seguinot (juseg.dev)
+# Copyright (c) 2024, Julien Seguinot (juseg.dev)
 # Creative Commons Attribution-ShareAlike 4.0 International License
 # (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
 
