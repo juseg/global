3125fbf5fef9546112ea64eb56602a0c8f5954f0
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2024-10-08 17:40:56 +0200

    Plot cell count in each exclusive region.

--- figures/glopdd_areas.py
+++ figures/glopdd_areas.py
@@ -49,26 +49,26 @@ def plot(source='cw5e5'):
         figsize=(160/25.4, 80/25.4), ncols=2, gridspec_kw={
             'left': 0.1, 'bottom': 0.15, 'right': 0.95, 'top': 0.9})
 
-    # define glacier regions
-    regions = {
-        'Antarctica': [-180, -90, 180, -60],
-        'Greenland': [-75, 60, -15, 90],
-        'World': [-180, -90, 180, 90]}
-
     # open inception threshold and elevation model
     with glopdd_utils.open_inception_threshold(source=source) as git:
 
         # select partial data for testing
-        label = 'Greenland'
-        west, south, east, north = regions[label]
-        git = git.sel(lat=slice(south, north), lon=slice(west, east))
+        git = git.isel(lat=slice(0, -1, 10), lon=slice(0, -1, 10))
 
-        # plot cumulative area
+        # loop on glaciated regions
         ax = axes[0]
-        cells = git.groupby(git).count()
-        cells = cells.reindex(git=cells.git[::-1])
-        cells = cells.cumsum(dim='git')
-        cells.plot(ax=ax, label=label)
+        regions = regions_like(git)
+        for label in regions.labels:
+
+            # select regional data
+            mask = regions == label
+            sel = git.where(mask.compute(), drop=True)
+
+            # plot cumulative area
+            cells = sel.groupby(sel).count()
+            cells = cells.reindex(git=cells.git[::-1])
+            cells = cells.cumsum(dim='git')
+            cells.plot(ax=axes[0], label=label)
 
         # set axes properties
         ax.legend()
