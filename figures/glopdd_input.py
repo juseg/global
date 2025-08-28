3735a7aebcbfbd3fc12ea5c2c057f0fcec42a59f
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2025-08-28 16:19:44 +0200

    Rework input figure to add precip and stdev.

--- figures/glopdd_input.py
+++ figures/glopdd_input.py
@@ -3,76 +3,57 @@
 # Creative Commons Attribution-ShareAlike 4.0 International License
 # (CC BY-SA 4.0, http://creativecommons.org/licenses/by-sa/4.0/)
 
-"""Plot global PDD input temperature side-by-side comparison."""
+"""Plot global PDD input temperature, precipitation and stdev."""
 
-import os.path
-import xarray as xr
-import matplotlib.pyplot as plt
+import sys
+import absplots as apl
 import glopdd_utils
 
-
-def open_climatology(month=7, source='cw5e5', var='tas'):
-    """Open input climatology from hyoga cache directory."""
-    prefix = os.path.join('~', '.cache', 'hyoga', source, 'clim', source)
-    da = xr.open_mfdataset(
-        f'{prefix}.{var}.mon.8110.avg.??0???0.nc',
-        chunks={}, decode_coords='all').to_array().squeeze()
-    # FIXME align coordinate names and values in hyoga?
-    if source == 'cera5':
-        da = da.assign_coords(month=range(1, 13)).rename(x='lon', y='lat')
-        da['lat'] = da.lat.astype('f4')
-        da['lon'] = da.lon.astype('f4')
-        da = da.sortby(da.lat, ascending=True)
-    if source == 'cw5e5' and var == 'tas':
-        da = da - 273.15
-    da = da.sel(month=month)
-    return da
+# FIXME align coordinate names and units within hyoga to avoid that import
+# FIXME this fails on relative paths (use ln -s ../data/external external)
+sys.path.append('../data/')
+import glopdd  # noqa pylint: disable=import-error, wrong-import-order, wrong-import-position
 
 
-def plot():
+def plot(source='cw5e5'):
     """Main program called during execution."""
 
     # initialize figure
-    fig, axes = plt.subplots(
-        figsize=(160/25.4, 80/25.4), ncols=3, gridspec_kw={
-            'left': 0.5/36, 'bottom': 0.5/18, 'right': 35.5/36,
-            'top': 16.5/18, 'wspace': 1/11})
-    cax1 = fig.add_axes([3/36, 3/18, 6/36, 0.5/18])
-    cax2 = fig.add_axes([27/36, 3/18, 6/36, 0.5/18])
-    for ax in (axes[0], axes[2]):
-        ax.add_patch(plt.Rectangle(
-            (0, 0), 1, 3.5/16, alpha=0.75, ec=str(2/3), fc='w',
-            transform=ax.transAxes))
+    fig, axes = apl.subplots_mm(
+        figsize=(160, 80), ncols=3, nrows=3, gridspec_kw={
+            'left': 2.5, 'right': 2.5, 'bottom': 17.5, 'top': 2.5,
+            'wspace': 2.5, 'hspace': 2.5, 'height_ratios': (5, 5, 1)})
 
-    # open july temperature from hyoga cache directory
-    with (
-            open_climatology(source='cera5') as cera5,
-            open_climatology(source='cw5e5') as cw5e5):
+    # open climatology
+    temp, prec, stdv = glopdd.open_climate_tile('n30e060', source=source)
+    temp = temp.sortby(temp.lat, ascending=True)
+    prec = prec.sortby(prec.lat, ascending=True)
+    stdv = stdv.sortby(stdv.lat, ascending=True)
 
-        # select region (aspect 16x11 = 8x5.5)
-        cera5 = cera5.sel(lat=slice(27, 43), lon=slice(74, 85))
-        cw5e5 = cw5e5.sel(lat=slice(27, 43), lon=slice(74, 85))
+    # plot climate variables
+    for axrow, month in zip(axes, (1, 7)):
+        indexers = {'month': month, 'lat': slice(32, 42), 'lon': slice(65, 85)}
+        kwargs = {'add_labels': False, 'add_colorbar': False}
+        temp_plot = temp.sel(indexers).plot.imshow(
+            ax=axrow[0], cmap='RdBu_r', vmin=-30, vmax=30, **kwargs)
+        prec_plot = prec.sel(indexers).plot.imshow(
+            ax=axrow[1], cmap='Greens', vmin=0, vmax=15, **kwargs)
+        stdv_plot = stdv.sel(indexers).plot.imshow(
+            ax=axrow[2], cmap='Purples', vmin=0, vmax=5, **kwargs)
 
-        # plot absolute values
-        for ax, git in zip(axes, (cera5, cw5e5)):
-            git.plot.imshow(
-                ax=ax, add_labels=False, cmap='Reds', vmin=-10, vmax=30,
-                cbar_ax=cax1, cbar_kwargs={
-                    'label': '1981 - 2010 July temp (°C)',
-                    'orientation': 'horizontal'})
-
-        # plot difference
-        diff = cw5e5 - cera5
-        diff.plot.imshow(
-            ax=axes[2], add_labels=False,
-            cmap='RdBu_r', cbar_ax=cax2, cbar_kwargs={
-                'label': 'difference (K)',
-                'orientation': 'horizontal'})
+    # add colorbars
+    fig.colorbar(
+        temp_plot, cax=axes[2, 0], label='surface air temperature (°C)',
+        extend='both', orientation='horizontal')
+    fig.colorbar(
+        prec_plot, cax=axes[2, 1], label=r'precipitation ($mm day^{-1}$)',
+        extend='max', orientation='horizontal')
+    fig.colorbar(
+        stdv_plot, cax=axes[2, 2], label='temp standard deviation (K)',
+        extend='max', orientation='horizontal')
 
     # set axes properties
-    titles = glopdd_utils.get_plot_labels(source='cdiff')
-    for ax, title in zip(axes, titles):
-        ax.set_title(title)
+    for ax in axes[:2].flat:
         ax.set_aspect('equal')
         ax.xaxis.set_visible(False)
         ax.yaxis.set_visible(False)
@@ -83,8 +64,9 @@ def plot():
 
 def main():
     """Main program called during execution."""
-    fig = plot()
-    fig.savefig(__file__[:-3], dpi='figure')
+    sources = ['cera5', 'cw5e5']
+    plotter = glopdd_utils.MultiPlotter(plot, sources=sources)
+    plotter()
 
 
 if __name__ == '__main__':
